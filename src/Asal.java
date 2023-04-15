f8d2e786bf99c10349ba1e14b2328d651d56adb3
iCanOzcan <icanozcan96@gmail.com>
2023-04-15 11:20:32 +0300

    asalSayi
    www.patika.dev practice 30

---
+++ src/Asal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Asal {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for(int i =2; i<=100; i++){
+            boolean asalMi=true;
+            for(int j=2; j<i; j++){
+                if(i%j==0){
+                    asalMi=false;
+                    break;
+                }
+            }
+            if(asalMi){
+                System.out.print(i+"\t");
+            }
+        }
+    }
+}
